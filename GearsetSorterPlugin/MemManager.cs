b31463e1c9e794f6007d73516ed7b9d94471027d
missing_nia <ardenprado@yahoo.com>
2022-04-06 02:39:34 -0700

    move GearsetClassJob to its own file

--- GearsetSorterPlugin/MemManager.cs
+++ GearsetSorterPlugin/MemManager.cs
@@ -215,51 +215,5 @@ namespace GearsetSorterPlugin
         private unsafe delegate char FileWriteDelegate(byte* pModule, char forceWrite);
 
         private static FileWriteDelegate? mdSaveFileDAT;
-
-        // GearsetEntry.ClassJob attribute values
-        // TODO: implement role sorting + a user config to customize role order (default is by implemenation order so it's fucked lmao)
-        public enum GearsetClassJob : byte
-        {
-            GLD = 0x01,
-            PUG = 0x02,
-            MRD = 0x03,
-            LNC = 0x04,
-            ARC = 0x05,
-            CNJ = 0x06,
-            THM = 0x07,
-            CRP = 0x08,
-            BSM = 0x09,
-            ARM = 0x0A,
-            GSM = 0x0B,
-            LTW = 0x0C,
-            WVR = 0x0D,
-            ALC = 0x0E,
-            CUL = 0x0F,
-            MIN = 0x10,
-            BTN = 0x11,
-            FSH = 0x12,
-            PLD = 0x13,
-            MNK = 0x14,
-            WAR = 0x15,
-            DRG = 0x16,
-            BRD = 0x17,
-            WHM = 0x18,
-            BLM = 0x19,
-            ACN = 0x1A,
-            SMN = 0x1B,
-            SCH = 0x1C,
-            ROG = 0x1D,
-            NIN = 0x1E,
-            MCH = 0x1F,
-            DRK = 0x20,
-            AST = 0x21,
-            SAM = 0x22,
-            RDM = 0x23,
-            BLU = 0x24,
-            GNB = 0x25,
-            DNC = 0x26,
-            RPR = 0x27,
-            SGE = 0x28
-        }
     }
 }
